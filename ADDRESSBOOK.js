871cdd5a4b326b164aedf8603331ad55699066ef
kandula-prakash <abhinayprakash143@gmail.com>
2021-09-28 08:03:50 +0530

    feat:addressBook

---
+++ ADDRESSBOOK.js
@@ -0,0 +1,16 @@
+//uc1 
+//class defination
+ class AddressBook {
+     //constructor
+     constructor (...params) {
+         this.firstName = params[0];
+         this.lasttName = params[1];
+         this.address = params[2];
+         this.city = params[3];
+         this.state = params[4];
+         this.zip = params[5];
+         this.phoneNumber = params[6];
+         this.email = params[7];
+
+     }
+ }
